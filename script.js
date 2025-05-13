e133b21c8ee8995286c3593d1260b9ad499be325
meerasrar <148849327+meerasrar@users.noreply.github.com>
2025-05-13 10:55:32 +0530

    my game zone
    play game

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you! Your message has been sent.");
+});document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Thank you! Your message has been sent.");
+  document.getElementById("audio1").play();
+});
